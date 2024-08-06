cf50e67341ee9421bf3de69f974f0efe7fe021bf
Weißer Hase <wei3erHase@protonmail.com>
2024-08-06 18:01:59 +0200

    feat: bumping keeper scripting utils

--- src/init.ts
+++ src/init.ts
@@ -1,5 +1,4 @@
-import {FlashbotsBundleProvider} from '@flashbots/ethers-provider-bundle';
-import {BlockListener, FlashbotsBroadcastor} from '@keep3r-network/keeper-scripting-utils';
+import {PrivateBroadcastor, getEnvVariable, BlockListener} from '@keep3r-network/keeper-scripting-utils';
 import {providers, Wallet, Contract} from 'ethers';
 import {getMainnetSdk} from '@dethcrypto/eth-sdk-client';
 import 'dotenv/config.js';
@@ -11,6 +10,7 @@ import 'dotenv/config.js';
 const CHAIN_ID = 1;
 const PRIORITY_FEE_IN_WEI = 2e9;
 const GAS_LIMIT = 10e6;
+const builders = ['https://rpc.titanbuilder.xyz/', 'https://rpc.beaverbuild.org/'];
 
 // Flashbots settings
 // Size of our batch of bundles
@@ -18,26 +18,26 @@ export const BURST_SIZE = 3;
 // Blocks into the future to send our first batch of bundles
 export const FUTURE_BLOCKS = 0;
 
-const provider = new providers.JsonRpcBatchProvider(process.env['RPC_MAINNET_HTTPS_URI']!);
-const txSigner = new Wallet(process.env['TX_SIGNER_MAINNET_PRIVATE_KEY']!, provider);
-const bundleSigner = new Wallet(process.env['BUNDLE_SIGNER_MAINNET_PRIVATE_KEY']!, provider);
-
-const buttplugWars = getMainnetSdk(txSigner).buttplugWars;
-
 (async () => {
-  const FLASHBOTS_RPC: string = process.env['FLASHBOTS_MAINNET_RELAYER']!;
-  const flashbotsProvider = await FlashbotsBundleProvider.create(provider, bundleSigner, FLASHBOTS_RPC);
-  const flashbotBroadcastor = new FlashbotsBroadcastor(flashbotsProvider, PRIORITY_FEE_IN_WEI, GAS_LIMIT);
+  // Environment variables usage
+  const provider = new providers.JsonRpcProvider(getEnvVariable('RPC_HTTP_MAINNET_URI'));
+  const txSigner = new Wallet(getEnvVariable('TX_SIGNER_PRIVATE_KEY'), provider);
+  
+  // Instantiates the contract
+  const buttplugWars = getMainnetSdk(txSigner).buttplugWars;
+
+  // Instantiates the broadcastor
+  const broadcastor = new PrivateBroadcastor(builders, PRIORITY_FEE_IN_WEI, GAS_LIMIT, true, CHAIN_ID);
 
   const blockListener = new BlockListener(provider);
 
   blockListener.stream(async (block) => {
     try{
-      await flashbotBroadcastor.tryToWorkOnFlashbots({
+      await broadcastor.tryToWork({
         jobContract: buttplugWars,
-        workMethod: 'executeMove()',
+        workMethod: 'executeMove',
         workArguments: [],
-        block
+        block: block
       });
     }
     catch (error: unknown) {
